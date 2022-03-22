468c5ce8e82af0af3f51859850f8b5f7f65e4937
SakuraMilkTea8 <73587894+SakuraMilkTea8@users.noreply.github.com>
2022-03-22 18:09:25 +0900

    Added a basic search engine with its corresponding state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      Hello
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>
+          This app was coded by{" "}
+          <a href="https://shr.link/3t82s" target="_blank" rel="noreferrer">
+            Ellie
+          </a>{" "}
+          , and is{" "}
+          <a
+            href="https://github.com/SakuraMilkTea8/Weather-App-React"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
